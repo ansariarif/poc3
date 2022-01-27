9224196f5ef5f8f04c4f47156bb4af7a20317144
ansariarif <ansariarif1007@gmail.com>
2022-01-27 17:44:18 +0530

    poc3

--- POC1/src/main/java/com/neosoft/Poc1/Controller/UserContoller.java
+++ POC1/src/main/java/com/neosoft/Poc1/Controller/UserContoller.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,10 +19,12 @@ import com.neosoft.Poc1.Service.InvalidRequestException;
 import com.neosoft.Poc1.Service.UserService;
 
 @RestController
+@CrossOrigin("http://localhost:4200")
 public class UserContoller {
 	
 	@Autowired
 	UserService userService;
+	
 	
 	@GetMapping("/users")
 	public List<User> getUsers(){
